d7254bf2d6793392553071e15885e628ba850513
danielklapp <daniel@klapp.name>
2022-08-15 15:26:26 -0400

    added untracked files

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd6ee3ba2-890b-4d4d-804d-454d6d01d3bd';
+export const MODAL_CLOSE_SEC = 2.5;
